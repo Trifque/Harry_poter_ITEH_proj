3cbc26b744e21c4207aaae90d6f81cc00cbe9a90
Aleksandar Trifunovic <aleksandar.trifunovich@gmail.com>
2023-09-12 13:18:02 +0200

    Napisali modele

--- blog-post/app/Models/Awarded_to.php
+++ blog-post/app/Models/Awarded_to.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Awarded_to extends Model
 {
     use HasFactory;
 
-    public function category(){ return $this->belongsTo(Category::class); }
+    public function award(){ return $this->belongsTo(Award::class); }
     public function user(){ return $this->belongsTo(User::class); }
-
+    public function post(){ return $this->belongsTo(Post::class); }
 }
